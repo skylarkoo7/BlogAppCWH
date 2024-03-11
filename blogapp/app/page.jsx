b2b600af23dd868676647194070d6fa6ea28c7e5
Neel <neelskylarkoo7@gmail.com>
2024-03-11 18:14:30 +0530

    Second Try

--- blogapp/app/page.jsx
+++ blogapp/app/page.jsx
@@ -1,10 +1,15 @@
-import { Client } from 'appwrite';
+import { Client } from "appwrite";
 
 export default function Home() {
+  const client = new Client();
+
+  client
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("65df5dc7006a72fb05ef");
   return (
-  <>
-    Navbar Here
-    <div></div>
-  </>
+    <>
+      Navbar Here
+      <div></div>
+    </>
   );
 }
